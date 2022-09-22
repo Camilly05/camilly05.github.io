647fd90f0edb3b0b0a2a6320a470ec10120c84a5
Camilly05 <103939425+Camilly05@users.noreply.github.com>
2022-09-22 14:57:15 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-let item = 0;                     //contador de imagens solicitadas
-const max = 79;                  //número da última imagem
-const updateRate = 2000;         //taxa de atualizção automática em ms
+let item = 0;       //contador de imagens solicitadas
+const max = 91;     //quantidade de imagens
+const updateRate = 2000;        //taxa de atualização automática em segundos
 
-function proxImagem( img ){ //solicita nova imagem via fetch
-fetch("img/"+img+".jpg")
-.then(resp => resp.blod())
-.then(blod =>{
-    const imagemObjteURL = URL.createObjectURL(blod);
-    console.log(imagemObjteURL);
-    const proxImg = document.createElement("img");
-    proxImg.src = imagemObjteURL;
-    document.getElementById("placeholder").appendChild(proxImg);
-})
+function proxImagem( img ){          //solicita nova imagem via fetch()
+    fetch("img/" + img + ".jpg")
+        .then(resp => resp.blob())
+        .then(blob => {
+            const imageObjectURL = URL.createObjectURL(blob);       //cria endereço da imagem
+            console.log(imageObjectURL);
+            const proxImg = document.createElement("img");
+            proxImg.src = imageObjectURL;
+            document.getElementById("placeholder").appendChild(proxImg);
+        })
 }
 
 window.onload = setInterval(function(){
@@ -19,3 +19,9 @@ window.onload = setInterval(function(){
     let scrollPoint = window.scrollY + window.innerHeight;
     window.scrollTo({top: scrollPoint, behavior: "smooth"});
 }, updateRate);
+
+window.onload = function(){
+    for( item = 0; item < 5; item++){
+        proxImagem(item);
+    }
+}
